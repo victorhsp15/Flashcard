46bc01255fe0c33e2ed1ad45c38d8d46105c68a7
victorhsp15 <00001110892895sp@al.educacao.sp.gov.br>
2024-11-18 11:31:40 -0300

    Adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem descubriu o Brasil?',
+    'Pedro Álvares Cabra'
+)
+
+criaCartao(
+    'Artes',
+    'Quando foi lançado o primeiro filme?',
+    '28 de dezembro de 1895'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
